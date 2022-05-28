6cae411ee9658e142d4b2f6e450a9fc608923f51
MiamiHacker <105590216+MiamiHacker@users.noreply.github.com>
2022-05-28 17:07:10 -0400

    founded mails and logins are stored in a .txt file
    check also on duplicated values in .txt file

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from bs4 import BeautifulSoup
 from colorama import Fore
 import requests
@@ -6,16 +8,26 @@ import urllib.parse
 from collections import deque
 import re
 
-target_url = str(input('[+] Enter Target URL To Scan: '))
+login_scan = True
+
+#target_url = str(input('[+] Enter Target URL To Scan: '))
+target_url = 'http://192.168.2.7/'
+# target_url = 'http://192.168.2.7/wp-login.php?'
+
 urls = deque([target_url])
 
 founded_urls = set()
 founded_emails = set()
+founded_logins = set()
+
 #set the maximum scan 
-maxScans = 25
+maxScans = 70
 count = 0
+
+j = 0
 try:
     while len(urls):
+        j += 1
         count += 1
         if count == maxScans:
             break
@@ -23,10 +35,23 @@ try:
         founded_urls.add(url)
 
         founded_parts = urllib.parse.urlsplit(url)
+        if "/wp-login.php" in url and login_scan:
+            with open("login.txt", "a+") as file:
+                file.seek(0) 
+                lines = file.read().splitlines() 
+                value = url
+                if value in lines:
+                    # print()
+                    pass
+                else:
+                    # write to file
+                    wr_value = value
+                    file.write(wr_value + "\n") # in append mode writes will always go to the end, so no need to seek() here
+                    print(Fore.YELLOW, "New loginpage stored:", Fore.GREEN, (url), Fore.RESET)
+
         base_url = '{0.scheme}://{0.netloc}'.format(founded_parts)
-
         path = url[:url.rfind('/')+1] if '/' in founded_parts.path else url
-
+ 
         print (Fore.RESET + '[%d] Crawling %s' % (count, url))
         try:
             response = requests.get(url)
@@ -35,9 +60,11 @@ try:
 
         new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
         founded_emails.update(new_emails)
-
+        
+        new_login = set(re.findall(r"[a-z0-9\.\-+_]+login[a-z0-9\.\-+_]+\.[a-z]+", response.text, re.I))
+        founded_logins.update(new_login)
+        
         soup = BeautifulSoup(response.text, features="lxml")
-
         for anchor in soup.find_all("a"):
             link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
             if link.startswith('/'):
@@ -46,8 +73,19 @@ try:
                 link = path + link
             if not link in urls and not link in founded_urls:
                 urls.append(link)
+
+        for mail in founded_emails:
+            value = mail
+            with open("mail.txt", "a+") as file:
+                file.seek(0) # set position to start of file
+                lines = file.read().splitlines() # now we won't have those newlines
+                if value in lines:
+                    # print()
+                    pass
+                else:
+                    wr_value = value
+                    file.write(wr_value + "\n") # in append mode writes will always go to the end, so no need to seek() here
+                    print(Fore.YELLOW, "New MailAddress stored:", Fore.GREEN, (mail), Fore.RESET)
+
 except KeyboardInterrupt:
     print('[-] Closing!')
-
-for mail in founded_emails:
-    print(Fore.YELLOW + "Founded:", mail)
